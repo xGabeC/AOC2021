6749965d17ecdeab2bca8880045af8a2d8c5375d
Gabe C <gcar2101@gmail.com>
2021-12-02 18:28:45 -0600

    Removed the hello world print...

--- Day 2/part_one/src/main.rs
+++ Day 2/part_one/src/main.rs
@@ -1,5 +1,4 @@
 fn main() {
-    println!("Hello, world!");
     let path = std::env::args().nth(1).expect("No path to file given!");
     let content = std::fs::read_to_string(path).expect("Could not read file.");
 
